12498838c67c9280ec6e2e119f0b452e1eca8cc8
sas1711092 <104480709+sas1711092@users.noreply.github.com>
2022-06-06 12:16:20 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+music.setVolume(127)
 basic.forever(function () {
     serial.writeLine("" + (sonar.ping(
     DigitalPin.P1,
@@ -9,4 +10,9 @@ basic.forever(function () {
     DigitalPin.P2,
     PingUnit.Centimeters
     ))
+})
+basic.forever(function () {
+    music.playTone(880, music.beat(BeatFraction.Half))
+    basic.pause(100)
+    music.playTone(988, music.beat(BeatFraction.Half))
 })
